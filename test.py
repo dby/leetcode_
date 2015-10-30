ba106036fdd57afa78600a75e029b64dbfb19a01
sys <1052856576@qq.com>
2015-10-31 00:07:42 +0800

    leetcode reset

--- test.py
+++
@@ -1,8 +0,0 @@
-def genMatrix(rows,cols):
-    matrix = [[0 for col in range(cols)] for row in range(rows)]
-    for i in range(rows):
-        for j in range(cols):
-            print matrix[i][j],
-        print '\n'
-
-genMatrix(5,5)
